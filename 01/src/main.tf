4a0461b5b5f08277a6c6ae70ff94574ce8ec65db
blade <vlad.rabbit@gmail.com>
2024-07-26 16:54:01 +0300

    HW 01

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -28,7 +28,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string.result}"
+  name  = "hello_world"
 
   ports {
     internal = 80
