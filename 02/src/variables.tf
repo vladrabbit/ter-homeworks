bb2ae71864f2307bf70a74939a4e94b922132f2f
vladrabbit <54140020+vladrabbit@users.noreply.github.com>
2024-08-06 21:08:24 +0300

    Add files via upload

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -33,7 +33,23 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "vm_web_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "VM image OS type"
+}
 
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "VM name"
+}
+
+variable "vm_web_platform" {
+  type        = string
+  default     = "standard-v1"
+  description = "VM processor family"
+}
 ###ssh vars
 
 variable "vms_ssh_root_key" {
