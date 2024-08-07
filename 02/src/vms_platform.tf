f9c0b312ea0d39b776c2c7be44a0b333eb09d3e4
vladrabbit <54140020+vladrabbit@users.noreply.github.com>
2024-08-07 14:07:59 +0300

    Add files via upload

---
+++ 02/src/vms_platform.tf
@@ -0,0 +1,77 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  default  = "b1gfrmq17gqpgc2qomso"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default = "b1gk7thjvss4509u20c3"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+variable "vm_db_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "VM image OS type"
+}
+
+variable "vm_db_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "VM name"
+}
+
+variable "vm_db_platform" {
+  type        = string
+  default     = "standard-v1"
+  description = "VM processor family"
+}
+
+variable "vm_db_cores" {
+  type        = number
+  default     = 2
+  description = "VM processor cores"
+}
+
+variable "vm_db_memory" {
+  type        = number
+  default     = 1
+  description = "VM memory"
+}
+
+variable "vm_db_fraction" {
+  type        = number
+  default     = 5
+  description = "VM processor fraction"
+}
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3GqZVhZlkqVLV77Uqt8O6m+Xs8x8kQuFibdsSCQEHw blade@blade-VirtualBox"
+  description = "ssh-keygen -t ed25519"
+}
