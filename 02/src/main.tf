5e2f43cb894771024b213548c1b89741db1434b3
blade <vlad.rabbit@gmail.com>
2024-08-07 13:40:38 +0300

    Update hw-02.md

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -10,11 +10,11 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_family 
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v1"
+  name        = var.vm_web_name 
+  platform_id = var.vm_web_platform 
   resources {
     cores         = 2
     memory        = 1
